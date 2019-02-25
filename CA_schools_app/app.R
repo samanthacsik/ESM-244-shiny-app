a0f7caf902a18153a02f4f5f5f39f756c6b6b406
samanthacsik <samanthacsik@gmail.com>
2019-02-24 19:19:54 -0800

    created app.R

---
+++ CA_schools_app/app.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  theme = shinytheme("flatly"),
+   
+   # Application title
+   titlePanel("Assessing the need for academic outreach initiatives across CA school districts "),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         tabsetPanel(
+           type = "tab",
+           tabPanel("Instructions"),
+           tabPanel("Enrollment by Race & Gender"),
+           tabPanel("College Preparedness Model"),
+           tabPanel("Data Explorer")
+         )
+      )
+   )
+)
+
+######################################################################
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
