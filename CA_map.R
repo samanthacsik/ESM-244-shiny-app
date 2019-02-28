0b70c97d4b640a9ee6949359ffb8014350526958
samanthacsik <samanthacsik@gmail.com>
2019-02-28 15:18:22 -0800

    stuff

--- CA_map.R
+++ CA_map.R
@@ -41,27 +41,62 @@ library(tmap)
 ##############################
 ca_counties <- st_read(dsn = ".", layer = "CA_Counties_TIGER2016") # "." means we're already in our wd; layer = "first common string name of each file used"
 
-write_csv(ca_counties, "ca_counties.csv")
+# write_csv(ca_counties, "ca_counties.csv")
 
-ca_secondary_districts <- st_read(dsn = ".", layer = "cb_2017_06_scsd_500k")  %>% 
-  rename(CODE = SCSDLEA)
+# https://www.census.gov/geo/maps-data/data/cbf/cbf_sd.html
 
-ca_unified_districts <- st_read(dsn = ".", layer = "cb_2017_06_unsd_500K") %>% 
-  rename(CODE = UNSDLEA)
+ca_elementary_districts <- st_read(dsn = ".", layer = "cb_2017_06_elsd_500k") %>% 
+  rename(CODE = ELSDLEA)
 
-ca_districts <- rbind(ca_secondary_districts, ca_unified_districts)
+ca_secondary_districts <- st_read(dsn = ".", layer = "cb_2017_06_scsd_500k")  %>%
+   rename(CODE = SCSDLEA)
+
+ca_unified_districts <- st_read(dsn = ".", layer = "cb_2017_06_unsd_500K") %>%
+   rename(CODE = UNSDLEA)
+
+ca_districts <- rbind(ca_elementary_districts, ca_secondary_districts, ca_unified_districts)
+
 
 ##############################
 # use geom_sf to make a map in ggplot
 ##############################
 
-ca_districts <- ggplot(ca_districts) +
-  geom_sf(lty = 1)  
-
-ca_districts
+# ca_counties_map <- ggplot(ca_counties) +
+#   geom_sf(lty = 1)
 
 ca_df_transform <- st_transform(ca_districts, crs = 4326)
 
-leaflet(ca_df_transform) %>% 
-  addTiles() %>% 
-  addPolygons()
+# leaflet(ca_df_transform) %>%
+#   addTiles() %>%
+#   addPolygons()
+
+#######################
+######################
+
+# create empty widget
+# map <- leaflet(width = 400, height = 400) %>% 
+#   addTiles(map) %>% 
+#   #addPolygons() %>% 
+#   setView(map, lng = 119.418,
+#                lat = 36.778,
+#                zoom = 5)
+# 
+# # select provider baselayers
+# maptypes <- c("MapQuestOpen.Aerial",
+#               "OpenStreetMap")
+# 
+# # use provider tiles
+# map <- leaflet() %>% 
+#   addProviderTiles(maptypes[1])
+
+# add  spatial data polygons and marker that can be clicked, dragged, or hovered over.
+map <- leaflet() %>% 
+  addProviderTiles("OpenStreetMap") %>% 
+  addPolygons(data = ca_df_transform, fillOpacity = 0.1, weight = 2) %>% 
+  addMarkers(lng = -119.4179,
+             lat = 36.7783, 
+             popup = "You are here.",
+             options = markerOptions(draggable = TRUE, riseOnHover = TRUE)) %>% 
+  setView(lng = -119.4179,
+          lat = 36.7783,
+          zoom = 6)
--- CA_schools_app3/ui.R
+++ CA_schools_app3/ui.R
@@ -31,7 +31,7 @@ edu_att_filter <- read_csv("edu_att_filter.csv")
          # panel 1 (instructions)
          tabPanel("Instructions",
                   h3("Academic outreach in California"),
-                  p("Academic outreach programs are often recognized as important initiatives for improving the rates of student retention (Quigley & Leon 2003) as well as increasing the percentage of students from underrepresented groups that advance through the academic pipeline (Coper et al., Gullatt & Jan 2003, Loza 2003). The University of California's Student Academic Preparation and Educational Partnership (SAPEP) is just one example and comprises a variety of programs to prepare CA students for postsecondary education, including students from socioeconomically disadvantaged backgrounds. This application is intented to be used as a tool for outreach coordinators in CA to target districts that could effectively utilize additional or continued educational program support to advance the academic success of their students. We explore data on race and gender composition across CA school districts and develop a model for predicting college preparedness based on both currently available resources and socioeconomic variables."),
+                  p("Academic outreach programs are often recognized as important initiatives for improving the rates of student retention (Quigley & Leon 2003) as well as increasing the percentage of students from underrepresented groups that advance through the academic pipeline (Cooper et al. 2002, Gullatt & Jan 2003, Loza 2003). The University of California's Student Academic Preparation and Educational Partnership (SAPEP) is just one example and comprises a variety of programs to prepare CA students for postsecondary education, including students from socioeconomically disadvantaged backgrounds. This application is intented to be used as a tool for outreach coordinators in CA to target districts that could effectively utilize additional or continued educational program support to advance the academic success of their students. We explore data on race and gender composition across CA school districts and develop a model for predicting college preparedness based on both currently available resources and socioeconomic variables."),
                   h3("Data sources"),
                   p("Data is made publically available by the California Department of Education"),
                   tags$a(href = "https://www.cde.ca.gov/ds/dd/index.asp", "Click here to access data")
@@ -40,8 +40,6 @@ edu_att_filter <- read_csv("edu_att_filter.csv")
          # panel 2 (map and income)
          tabPanel("Population & Income",
                   fluidRow(
-                    column(4, "outputs go here"),
-                    column(8, "map goes here")
                   )),
          
          # panel 3 ()
